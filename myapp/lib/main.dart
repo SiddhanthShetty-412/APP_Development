c9592b0fbe546bbbf33ff8e1f125a221c8c6c5bd
SHETTY SIDDHANTH SATHISH <siddhanth412@gmail.com>
2026-07-13 11:32:33 +0000

    Updated login screen

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,15 +1,16 @@
 import "package:flutter/material.dart";
 import "register_screen.dart";
+
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginScreen(),
-    );
+    return MaterialApp(home: const LoginScreen());
   }
 }
 
@@ -19,6 +20,7 @@ class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
+
 class _LoginScreenState extends State<LoginScreen> {
   final formKey = GlobalKey<FormState>();
 
@@ -27,35 +29,34 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void login() {
     if (formKey.currentState!.validate()) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Login Successful")),
-      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Login Successful")));
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Login Screen")),
-        body:Center(
-          child: Container(
-            padding:EdgeInsets.all(16.0),
-            margin: EdgeInsets.all(16.0),
-            height: 300,
-            width: 500,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 2),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Form(
-              key: formKey,
-              child: Column(
+      appBar: AppBar(title: Text("Login Screen")),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          margin: EdgeInsets.all(16.0),
+          height: 300,
+          width: 500,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black, width: 2),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Form(
+            key: formKey,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFormField(
                   controller: nameController,
-                  decoration: InputDecoration(
-                    labelText: "Username",
-                  ),
+                  decoration: InputDecoration(labelText: "Username"),
                   validator: (value) {
                     if (value!.isEmpty) {
                       return 'Please enter username';
@@ -67,32 +68,30 @@ class _LoginScreenState extends State<LoginScreen> {
                 TextFormField(
                   controller: passwordController,
                   obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                  ),
+                  decoration: InputDecoration(labelText: "Password"),
                   validator: (value) {
-                        if (value!.isEmpty || value.length < 6) {
-                          return 'Please enter your password';
-                        }
-                        if (!RegExp(r'[a-z]').hasMatch(value)) {
-                          return 'Password must have at least one lowercase letter';
-                        }
-                        if (!RegExp(r'[A-Z]').hasMatch(value)){
-                          return 'Password must have at least one uppercase letter';
-                        }
-                        if (!RegExp(r'[0-9]').hasMatch(value)) {
-                          return 'Password must have at least one number';
-                        }
-                        if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
-                          return 'Password must have at least one special character';
-                        }
-                        return null;
-                  }
+                    if (value!.isEmpty || value.length < 6) {
+                      return 'Please enter your password';
+                    }
+                    if (!RegExp(r'[a-z]').hasMatch(value)) {
+                      return 'Password must have at least one lowercase letter';
+                    }
+                    if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                      return 'Password must have at least one uppercase letter';
+                    }
+                    if (!RegExp(r'[0-9]').hasMatch(value)) {
+                      return 'Password must have at least one number';
+                    }
+                    if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
+                      return 'Password must have at least one special character';
+                    }
+                    return null;
+                  },
                 ),
                 SizedBox(height: 16.0),
                 ElevatedButton(
                   onPressed: () {
-                      login();
+                    login();
                   },
                   child: Text("Login"),
                 ),
@@ -101,19 +100,16 @@ class _LoginScreenState extends State<LoginScreen> {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(
-                        builder: (context)=> RegisterScreen()
-                      ),
+                      MaterialPageRoute(builder: (context) => RegisterScreen()),
                     );
                   },
                   child: Text("Don't have an account? Register"),
                 ),
               ],
             ),
-            ),
-            
           ),
         ),
-      );
+      ),
+    );
   }
 }
